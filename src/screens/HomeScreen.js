9eea6d47f2edda796d2c92c21de2fe68c021c43c
sourav <souravramesh39@gmail.com>
2024-10-28 23:45:40 +0530

    Added school image as background and changed layout of home screen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,9 +3,10 @@ import {
   Text,
   SafeAreaView,
   TouchableOpacity,
-  useWindowDimensions,
   Alert,
   StyleSheet,
+  ScrollView,
+  ImageBackground,
 } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
@@ -13,7 +14,6 @@ import EvilIcons from "@expo/vector-icons/EvilIcons";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
 export default function HomeScreen({ navigation }) {
-  const { width } = useWindowDimensions();
   const handleLogout = () => {
     navigation.navigate("Login");
     Alert.alert("Logged out", "You have been successfully logged out");
@@ -22,7 +22,11 @@ export default function HomeScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
-      <View style={styles.header}>
+      <ImageBackground
+        source={require("../../assets/school-img.jpg")}
+        imageStyle={{ opacity: 0.1 }}
+        style={styles.header}
+      >
         <View style={styles.headerRow}>
           <View>
             <EvilIcons name="navicon" size={30} color="white" />
@@ -39,81 +43,83 @@ export default function HomeScreen({ navigation }) {
           <Text style={styles.welcomeText}>Welcome Sourav!</Text>
           <Text style={styles.schoolName}>CMI Public School</Text>
         </View>
-      </View>
+      </ImageBackground>
 
-      <View style={styles.gridContainer}>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.blueBackground]}>
-            <Ionicons name="person-outline" size={40} color="blue" />
-          </View>
-          <Text style={styles.gridItemText}>Student</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.redBackground]}>
-            <Ionicons name="cash-outline" size={40} color="red" />
-          </View>
-          <Text style={styles.gridItemText}>Fees</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.greenBackground]}>
-            <Ionicons name="book-outline" size={40} color="green" />
-          </View>
-          <Text style={styles.gridItemText}>Assignment</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.orangeBackground]}>
-            <Ionicons name="document-outline" size={40} color="orange" />
-          </View>
-          <Text style={styles.gridItemText}>Circulars</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.tealBackground]}>
-            <Ionicons name="time-outline" size={40} color="teal" />
-          </View>
-          <Text style={styles.gridItemText}>Attendance</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.violetBackground]}>
-            <Ionicons name="calendar-outline" size={40} color="violet" />
-          </View>
-          <Text style={styles.gridItemText}>Calendar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.indigoBackground]}>
-            <FontAwesome name="table" size={40} color="indigo" />
-          </View>
-          <Text style={styles.gridItemText}>Exam Time Table</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.blueBackground]}>
-            <FontAwesome name="table" size={40} color="blue" />
-          </View>
-          <Text style={styles.gridItemText}>Exam portions</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.redBackground]}>
-            <Ionicons name="document-outline" size={40} color="red" />
-          </View>
-          <Text style={styles.gridItemText}>Progress report</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.greenBackground]}>
-            <FontAwesome name="table" size={40} color="green" />
-          </View>
-          <Text style={styles.gridItemText}>Class Time Table</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.orangeBackground]}>
-            <Ionicons name="document-text-outline" size={40} color="orange" />
-          </View>
-          <Text style={styles.gridItemText}>Question Paper</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.gridItem, { width: width / 3 }]}>
-          <View style={[styles.iconContainer, styles.tealBackground]}>
-            <FontAwesome name="question-circle-o" size={40} color="teal" />
-          </View>
-          <Text style={styles.gridItemText}>Queries</Text>
-        </TouchableOpacity>
+      <View style={{ flex: 1 }}>
+        <ScrollView contentContainerStyle={styles.gridContainer}>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.blueBackground]}>
+              <Ionicons name="person-outline" size={40} color="blue" />
+            </View>
+            <Text style={styles.gridItemText}>Student</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.redBackground]}>
+              <Ionicons name="cash-outline" size={40} color="red" />
+            </View>
+            <Text style={styles.gridItemText}>Fees</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.greenBackground]}>
+              <Ionicons name="book-outline" size={40} color="green" />
+            </View>
+            <Text style={styles.gridItemText}>Assignment</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.orangeBackground]}>
+              <Ionicons name="document-outline" size={40} color="orange" />
+            </View>
+            <Text style={styles.gridItemText}>Circulars</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.tealBackground]}>
+              <Ionicons name="time-outline" size={40} color="teal" />
+            </View>
+            <Text style={styles.gridItemText}>Attendance</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.violetBackground]}>
+              <Ionicons name="calendar-outline" size={40} color="violet" />
+            </View>
+            <Text style={styles.gridItemText}>Calendar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.indigoBackground]}>
+              <FontAwesome name="table" size={40} color="indigo" />
+            </View>
+            <Text style={styles.gridItemText}>Exam Time Table</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.blueBackground]}>
+              <FontAwesome name="table" size={40} color="blue" />
+            </View>
+            <Text style={styles.gridItemText}>Exam portions</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.redBackground]}>
+              <Ionicons name="document-outline" size={40} color="red" />
+            </View>
+            <Text style={styles.gridItemText}>Progress report</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.greenBackground]}>
+              <FontAwesome name="table" size={40} color="green" />
+            </View>
+            <Text style={styles.gridItemText}>Class Time Table</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.orangeBackground]}>
+              <Ionicons name="document-text-outline" size={40} color="orange" />
+            </View>
+            <Text style={styles.gridItemText}>Question Paper</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.gridItem}>
+            <View style={[styles.iconContainer, styles.tealBackground]}>
+              <FontAwesome name="question-circle-o" size={40} color="teal" />
+            </View>
+            <Text style={styles.gridItemText}>Queries</Text>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -125,7 +131,7 @@ const styles = StyleSheet.create({
   },
   header: {
     backgroundColor: "#3b82f6",
-    flex: 1,
+    height: 250,
     padding: 16,
     paddingLeft: 24,
     justifyContent: "space-between",
@@ -133,6 +139,7 @@ const styles = StyleSheet.create({
   headerRow: {
     flexDirection: "row",
     justifyContent: "space-between",
+    marginTop: 18,
   },
   headerTitle: {
     color: "white",
@@ -165,7 +172,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "center",
-    flex: 3,
   },
   gridItem: {
     borderColor: "#d1d5db",
@@ -173,6 +179,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
+    width: "50%",
   },
   iconContainer: {
     borderRadius: 50,
